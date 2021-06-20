549627d0e2ac55b9ffbcf2decb716134fe1a7501
hlemont <hlemont01@gmail.com>
2021-06-20 15:23:24 +0900

    convert indention to spaces

--- app/OBS.ts
+++ app/OBS.ts
@@ -2,301 +2,301 @@ import { response, Router } from "express";
 import OBSWebSocket from "obs-websocket-js";
 
 export type Config = {
-	address: string;
-	port: number;
-	password: string;
-	sceneAliases: { [name: string]: string };
+  address: string;
+  port: number;
+  password: string;
+  sceneAliases: { [name: string]: string };
 };
 
 export default class OBS {
-	private config: Config;
-	private isConnected: boolean;
-	private obs: OBSWebSocket;
-	public sceneRouter: Router;
-	public streamRouter: Router;
-	public recordRouter: Router;
-	public router: Router;
+  private config: Config;
+  private isConnected: boolean;
+  private obs: OBSWebSocket;
+  public sceneRouter: Router;
+  public streamRouter: Router;
+  public recordRouter: Router;
+  public router: Router;
 
-	constructor(config: Config) {
-		this.config = config;
-		this.isConnected = false;
-		this.obs = new OBSWebSocket();
-		this.obs.on("ConnectionOpened", () => {
-			this.isConnected = true;
-			console.log("Server connected to OBS websocket");
-		});
-		this.obs.on("ConnectionClosed", () => {
-			this.isConnected = false;
-			console.log("Server disconnected from OBS websocket");
-		});
+  constructor(config: Config) {
+    this.config = config;
+    this.isConnected = false;
+    this.obs = new OBSWebSocket();
+    this.obs.on("ConnectionOpened", () => {
+      this.isConnected = true;
+      console.log("Server connected to OBS websocket");
+    });
+    this.obs.on("ConnectionClosed", () => {
+      this.isConnected = false;
+      console.log("Server disconnected from OBS websocket");
+    });
 
-		this.sceneRouter = this.initSceneRouter();
-		this.streamRouter = this.initStreamRouter();
-		this.recordRouter = this.initRecordRouter();
-		this.router = Router();
-		this.router.use("/scene", this.sceneRouter);
-		this.router.use("/stream", this.streamRouter);
-		this.router.use("/record", this.recordRouter);
+    this.sceneRouter = this.initSceneRouter();
+    this.streamRouter = this.initStreamRouter();
+    this.recordRouter = this.initRecordRouter();
+    this.router = Router();
+    this.router.use("/scene", this.sceneRouter);
+    this.router.use("/stream", this.streamRouter);
+    this.router.use("/record", this.recordRouter);
 
-		console.log(
-			`obs.config: ${JSON.stringify(
-				{
-					...this.config,
-					password: [...this.config.password].reduce(
-						(prev, curr) => prev + "*",
-						""
-					),
-					sceneAliases: undefined,
-				},
-				undefined,
-				1
-			)}`
-		);
-	}
+    console.log(
+      `obs.config: ${JSON.stringify(
+        {
+          ...this.config,
+          password: [...this.config.password].reduce(
+            (prev, curr) => prev + "*",
+            ""
+          ),
+          sceneAliases: undefined,
+        },
+        undefined,
+        1
+      )}`
+    );
+  }
 
-	public setConfig(newConfig: Config) {
-		this.config = newConfig;
-	}
+  public setConfig(newConfig: Config) {
+    this.config = newConfig;
+  }
 
-	private initSceneRouter() {
-		const router = Router();
+  private initSceneRouter() {
+    const router = Router();
 
-		router
-			.route("")
-			.get((req, res): void => {
-				this.connect()
-					.then(() => {
-						return this.obs.send("GetSceneList");
-					})
-					.then((response) => {
-						const names = response.scenes
-							.map((scene) => scene.name)
-							.map((name) => {
-								if (Object.values(this.config.sceneAliases).includes(name)) {
-									for (let alias in this.config.sceneAliases) {
-										if (name == this.config.sceneAliases[alias]) {
-											return alias;
-										}
-									}
-								}
-								return name;
-							});
-						console.log(
-							`Successfully got Scene list: ${names.slice(0, 3).join(", ")}`
-						);
-						res.json({ list: names });
-					})
-					.catch((response) => {
-						console.log(response);
-						res.status(500).json({ error: response.error });
-					});
-			})
-			.all((req, res) => {
-				res.status(405).send();
-			});
+    router
+      .route("")
+      .get((req, res): void => {
+        this.connect()
+          .then(() => {
+            return this.obs.send("GetSceneList");
+          })
+          .then((response) => {
+            const names = response.scenes
+              .map((scene) => scene.name)
+              .map((name) => {
+                if (Object.values(this.config.sceneAliases).includes(name)) {
+                  for (let alias in this.config.sceneAliases) {
+                    if (name == this.config.sceneAliases[alias]) {
+                      return alias;
+                    }
+                  }
+                }
+                return name;
+              });
+            console.log(
+              `Successfully got Scene list: ${names.slice(0, 3).join(", ")}`
+            );
+            res.json({ list: names });
+          })
+          .catch((response) => {
+            console.log(response);
+            res.status(500).json({ error: response.error });
+          });
+      })
+      .all((req, res) => {
+        res.status(405).send();
+      });
 
-		router
-			.route("/current")
-			.get((req, res): void => {
-				this.connect()
-					.then(() => {
-						return this.obs.send("GetCurrentScene");
-					})
-					.then((response) => {
-						let { name } = response;
-						if (Object.values(this.config.sceneAliases).includes(name)) {
-							for (let alias in this.config.sceneAliases) {
-								if (this.config.sceneAliases[alias] === name) {
-									name = alias;
-									break;
-								}
-							}
-						}
+    router
+      .route("/current")
+      .get((req, res): void => {
+        this.connect()
+          .then(() => {
+            return this.obs.send("GetCurrentScene");
+          })
+          .then((response) => {
+            let { name } = response;
+            if (Object.values(this.config.sceneAliases).includes(name)) {
+              for (let alias in this.config.sceneAliases) {
+                if (this.config.sceneAliases[alias] === name) {
+                  name = alias;
+                  break;
+                }
+              }
+            }
 
-						console.log(`Current scene is: ${name}`);
-						res.json({
-							name,
-						});
-					})
-					.catch((response) => {
-						console.log(response);
-						res.status(500).json({ error: response.error });
-					});
-			})
-			.post((req, res): void => {
-				if (!req.is("application/json")) {
-					res
-						.status(400)
-						.json({ error: "Content type should be application/json" });
-					return;
-				}
-				const sceneName =
-					req.body.name in this.config.sceneAliases
-						? this.config.sceneAliases[req.body.name]
-						: req.body.name;
+            console.log(`Current scene is: ${name}`);
+            res.json({
+              name,
+            });
+          })
+          .catch((response) => {
+            console.log(response);
+            res.status(500).json({ error: response.error });
+          });
+      })
+      .post((req, res): void => {
+        if (!req.is("application/json")) {
+          res
+            .status(400)
+            .json({ error: "Content type should be application/json" });
+          return;
+        }
+        const sceneName =
+          req.body.name in this.config.sceneAliases
+            ? this.config.sceneAliases[req.body.name]
+            : req.body.name;
 
-				this.connect()
-					.then(() => {
-						return this.obs.send("SetCurrentScene", {
-							"scene-name": sceneName,
-						});
-					})
-					.then(() => {
-						console.log(`Current scene successfully set to: ${sceneName}`);
-						res.status(204).send();
-					})
-					.catch((response) => {
-						console.log(response);
-						if (response.error === "requested scene does not exist") {
-							res.status(404).json({ error: response.error });
-						} else {
-							res.status(500).json({ error: response.error });
-						}
-					});
-			})
-			.all((req, res) => {
-				res.status(405).send();
-			});
+        this.connect()
+          .then(() => {
+            return this.obs.send("SetCurrentScene", {
+              "scene-name": sceneName,
+            });
+          })
+          .then(() => {
+            console.log(`Current scene successfully set to: ${sceneName}`);
+            res.status(204).send();
+          })
+          .catch((response) => {
+            console.log(response);
+            if (response.error === "requested scene does not exist") {
+              res.status(404).json({ error: response.error });
+            } else {
+              res.status(500).json({ error: response.error });
+            }
+          });
+      })
+      .all((req, res) => {
+        res.status(405).send();
+      });
 
-		return router;
-	}
+    return router;
+  }
 
-	private initStreamRouter() {
-		const router = Router();
+  private initStreamRouter() {
+    const router = Router();
 
-		router
-			.route("")
-			.get((req, res) => {
-				this.connect()
-					.then(() => {
-						return this.obs.send("GetStreamingStatus");
-					})
-					.then((response) => {
-						console.log(
-							`current streaming status: ${JSON.stringify(
-								response,
-								undefined,
-								1
-							)}`
-						);
-						res.json({ status: response });
-					})
-					.catch((response) => {
-						console.log(response);
-						res.json({ error: response.error });
-					});
-			})
-			.post((req, res) => {
-				if (!req.is("application/json")) {
-					res
-						.status(400)
-						.json({ error: "Content type should be application/json" });
-					return;
-				}
+    router
+      .route("")
+      .get((req, res) => {
+        this.connect()
+          .then(() => {
+            return this.obs.send("GetStreamingStatus");
+          })
+          .then((response) => {
+            console.log(
+              `current streaming status: ${JSON.stringify(
+                response,
+                undefined,
+                1
+              )}`
+            );
+            res.json({ status: response });
+          })
+          .catch((response) => {
+            console.log(response);
+            res.json({ error: response.error });
+          });
+      })
+      .post((req, res) => {
+        if (!req.is("application/json")) {
+          res
+            .status(400)
+            .json({ error: "Content type should be application/json" });
+          return;
+        }
 
-				const action: "start" | "stop" | "toggle" = req.body.action;
-				const request =
-					action === "start"
-						? "StartStreaming"
-						: action === "stop"
-						? "StopStreaming"
-						: action === "toggle"
-						? "StartStopStreaming"
-						: undefined;
+        const action: "start" | "stop" | "toggle" = req.body.action;
+        const request =
+          action === "start"
+            ? "StartStreaming"
+            : action === "stop"
+            ? "StopStreaming"
+            : action === "toggle"
+            ? "StartStopStreaming"
+            : undefined;
 
-				this.connect()
-					.then(() => {
-						if (request) {
-							return this.obs.send(request);
-						} else {
-							return Promise.reject({ error: `unknown action: ${action}` });
-						}
-					})
-					.then(() => {
-						console.log(`Stream [${action}]`);
-						res.status(204).send();
-					})
-					.catch((response) => {
-						console.log(response);
-						if (response.error === `unknown action: ${action}`) {
-							res.status(400).json({ error: response.error });
-						} else {
-							res.status(500).json({ error: response.error });
-						}
-					});
-			})
-			.all((req, res) => {
-				res.status(405).send();
-			});
+        this.connect()
+          .then(() => {
+            if (request) {
+              return this.obs.send(request);
+            } else {
+              return Promise.reject({ error: `unknown action: ${action}` });
+            }
+          })
+          .then(() => {
+            console.log(`Stream [${action}]`);
+            res.status(204).send();
+          })
+          .catch((response) => {
+            console.log(response);
+            if (response.error === `unknown action: ${action}`) {
+              res.status(400).json({ error: response.error });
+            } else {
+              res.status(500).json({ error: response.error });
+            }
+          });
+      })
+      .all((req, res) => {
+        res.status(405).send();
+      });
 
-		return router;
-	}
+    return router;
+  }
 
-	private initRecordRouter() {
-		const router = Router();
+  private initRecordRouter() {
+    const router = Router();
 
-		router
-			.route("")
-			.post((req, res) => {
-				if (!req.is("application/json")) {
-					res.status(400).send("bad request");
-					return;
-				}
-				const action: "start" | "stop" | "toggle" = req.body.action;
-				const request =
-					action === "start"
-						? "StartRecording"
-						: action === "stop"
-						? "StopRecording"
-						: action === "toggle"
-						? "StartStopRecording"
-						: undefined;
+    router
+      .route("")
+      .post((req, res) => {
+        if (!req.is("application/json")) {
+          res.status(400).send("bad request");
+          return;
+        }
+        const action: "start" | "stop" | "toggle" = req.body.action;
+        const request =
+          action === "start"
+            ? "StartRecording"
+            : action === "stop"
+            ? "StopRecording"
+            : action === "toggle"
+            ? "StartStopRecording"
+            : undefined;
 
-				this.connect()
-					.then(() => {
-						if (request) {
-							return this.obs.send(request);
-						} else {
-							return Promise.reject({ error: `unknown action: ${action}` });
-						}
-					})
-					.then(() => {
-						console.log(`Record [${action}]`);
-						res.status(204).send();
-					})
-					.catch((response) => {
-						console.log(response);
-						if (response.error === `unknown action: ${action}`) {
-							res.status(400).json({ error: response.error });
-						} else {
-							res.status(500).json({ error: response.error });
-						}
-					});
-			})
-			.all((req, res) => {
-				res.status(405).send();
-			});
+        this.connect()
+          .then(() => {
+            if (request) {
+              return this.obs.send(request);
+            } else {
+              return Promise.reject({ error: `unknown action: ${action}` });
+            }
+          })
+          .then(() => {
+            console.log(`Record [${action}]`);
+            res.status(204).send();
+          })
+          .catch((response) => {
+            console.log(response);
+            if (response.error === `unknown action: ${action}`) {
+              res.status(400).json({ error: response.error });
+            } else {
+              res.status(500).json({ error: response.error });
+            }
+          });
+      })
+      .all((req, res) => {
+        res.status(405).send();
+      });
 
-		return router;
-	}
+    return router;
+  }
 
-	public connect() {
-		return new Promise<boolean>((resolve, reject) => {
-			if (this.isConnected) {
-				resolve(true);
-			} else {
-				this.obs
-					.connect({
-						address: `${this.config.address}:${this.config.port}`,
-						password: this.config.password,
-					})
-					.then(() => {
-						resolve(false);
-					})
-					.catch((response) => {
-						reject({ ...response, error: response.error});
-					});
-			}
-		});
-	}
+  public connect() {
+    return new Promise<boolean>((resolve, reject) => {
+      if (this.isConnected) {
+        resolve(true);
+      } else {
+        this.obs
+          .connect({
+            address: `${this.config.address}:${this.config.port}`,
+            password: this.config.password,
+          })
+          .then(() => {
+            resolve(false);
+          })
+          .catch((response) => {
+            reject({ ...response, error: response.error});
+          });
+      }
+    });
+  }
 }
--- app/Remote.ts
+++ app/Remote.ts
@@ -2,219 +2,219 @@ import { Router } from "express";
 import robotjs from "robotjs";
 
 export enum ControlType {
-	keyTapping = "key",
-	stringTyping = "string",
-	delaying = "delay",
+  keyTapping = "key",
+  stringTyping = "string",
+  delaying = "delay",
 }
 
 export type Control =
-	| {
-			type: ControlType.keyTapping;
-			key: string;
-			modifiers: string[];
-	  }
-	| {
-			type: ControlType.stringTyping;
-			string: string;
-	  }
-	| {
-			type: ControlType.delaying;
-			delay: number;
-	  };
+  | {
+      type: ControlType.keyTapping;
+      key: string;
+      modifiers: string[];
+    }
+  | {
+      type: ControlType.stringTyping;
+      string: string;
+    }
+  | {
+      type: ControlType.delaying;
+      delay: number;
+    };
 
 export type Config = {
-	allowed: boolean;
-	macros: {
-		[macroName: string]: Control[];
-	};
+  allowed: boolean;
+  macros: {
+    [macroName: string]: Control[];
+  };
 };
 
 export default class Remote {
-	private config: Config;
-	public remoteRouter: Router;
+  private config: Config;
+  public remoteRouter: Router;
 
-	constructor(config: Config) {
-		this.config = config;
-		this.remoteRouter = this.initRemoteRouter();
-		robotjs.setKeyboardDelay(1);
+  constructor(config: Config) {
+    this.config = config;
+    this.remoteRouter = this.initRemoteRouter();
+    robotjs.setKeyboardDelay(1);
 
-		console.log(
-			`remote.config: ${JSON.stringify(
-				{ ...this.config, macros: undefined },
-				undefined,
-				1
-			)}`
-		);
-	}
+    console.log(
+      `remote.config: ${JSON.stringify(
+        { ...this.config, macros: undefined },
+        undefined,
+        1
+      )}`
+    );
+  }
 
-	public initRemoteRouter() {
-		const router = Router();
+  public initRemoteRouter() {
+    const router = Router();
 
-		
-		router
-			.route("/control")
-			.post((req, res) => {
-				if (!req.is("application/json")) {
-					res
-						.status(400)
-						.json({ error: "Content type should be application/json" });
-					return;
-				}
+    
+    router
+      .route("/control")
+      .post((req, res) => {
+        if (!req.is("application/json")) {
+          res
+            .status(400)
+            .json({ error: "Content type should be application/json" });
+          return;
+        }
 
-				if (!this.config.allowed) {
-					res.status(401).json({ error: "remote control not allowed" });
-					return;
-				}
-
-
-				const control: Control = req.body.control;
-				if(!control){
-			    res.status(400).json({ error: "missing request parameter: control"});
-				}
-
-				console.log(`remote control:${JSON.stringify(control, undefined, 1)}`);
+        if (!this.config.allowed) {
+          res.status(401).json({ error: "remote control not allowed" });
+          return;
+        }
 
 
-				this.runControl(control)
-					.then(() => res.status(204).send())
-					.catch((error) =>
-						(error.split(":")[0].split(" ")[0] === "invalid"
-							? res.status(400)
-							: res.status(500)
-						).json({ error })
-					);
-			})
-			.all((req, res) => {
-				res.status(405).send();
-			});
+        const control: Control = req.body.control;
+        if(!control){
+          res.status(400).json({ error: "missing request parameter: control"});
+        }
+
+        console.log(`remote control:${JSON.stringify(control, undefined, 1)}`);
 
 
-		router
-			.route("/macro")
-			.get((req, res) => {
-				if (!this.config.allowed) {
-					res.status(401).json({ error: "remote control not allowed" });
-					return;
-				}
-				res.json({ macros: this.config.macros });
-			})
-			.post((req, res) => {
-				if (!req.is("application/json")) {
-					res
-						.status(400)
-						.json({ error: "Content type should be application/json" });
-					return;
-				}
-
-				if (!this.config.allowed) {
-					res.status(401).json({ error: "remote control not allowed" });
-					return;
-				}
-
-				const macro: Control[] = req.body.macro;
-				if(!macro){
-			    res.status(400).json({ error: "missing request parameters: macro"});
-				}
-
-				console.log(`remote macro:${JSON.stringify(macro, undefined, 1)}`);
+        this.runControl(control)
+          .then(() => res.status(204).send())
+          .catch((error) =>
+            (error.split(":")[0].split(" ")[0] === "invalid"
+              ? res.status(400)
+              : res.status(500)
+            ).json({ error })
+          );
+      })
+      .all((req, res) => {
+        res.status(405).send();
+      });
 
 
-				this.runMacro(macro)
-					.then(() => res.status(204).send())
-					.catch((error) =>
-					(error.split(":")[0].split(" ")[0] === "invalid"
-						? res.status(400)
-						: res.status(500)
-					).json({ error })
-				);
-			})
-			.all((req, res) => {
-				res.status(405).send();
-			});
+    router
+      .route("/macro")
+      .get((req, res) => {
+        if (!this.config.allowed) {
+          res.status(401).json({ error: "remote control not allowed" });
+          return;
+        }
+        res.json({ macros: this.config.macros });
+      })
+      .post((req, res) => {
+        if (!req.is("application/json")) {
+          res
+            .status(400)
+            .json({ error: "Content type should be application/json" });
+          return;
+        }
 
-		router
-			.route("/macro/:name")
-			.get((req, res) => {
-				if (!this.config.allowed) {
-					res.status(401).json({ error: "remote control not allowed" });
-					return;
-				}
+        if (!this.config.allowed) {
+          res.status(401).json({ error: "remote control not allowed" });
+          return;
+        }
 
-				const { name: macroName } = req.params;
-				if (macroName in this.config.macros) {
-					res.json({
-						macro: this.config.macros[macroName],
-					});
-				} else {
-					res.status(404).json({ error: `macro not found: ${macroName}` });
-				}
-			})
-			.post((req, res) => {
-				if (!this.config.allowed) {
-					res.status(401).json({ error: "remote control not allowed" });
-					return;
-				}
+        const macro: Control[] = req.body.macro;
+        if(!macro){
+          res.status(400).json({ error: "missing request parameters: macro"});
+        }
 
-				const { name: macroName } = req.params;
-				if (req.params.name in this.config.macros) {
-					const macro = this.config.macros[macroName];
-					this.runMacro(macro)
-						.then(() => res.status(204).send())
-						.catch((error: Error) => res.status(500).json({ error }));
-				}
-			})
-			.all((req, res) => {
-				res.status(405).send();
-			});
+        console.log(`remote macro:${JSON.stringify(macro, undefined, 1)}`);
 
-		return router;
-	}
 
-	private isValidControl(control: Control) {
-		if (control.type === ControlType.keyTapping) {
-			return control.key !== undefined && typeof control.key === "string" && control.key !== "";
-		} else if (control.type === ControlType.stringTyping) {
-			return control.string !== undefined && typeof control.string === "string";
-		} else if (control.type === ControlType.delaying) {
-			return control.delay !== undefined && !isNaN(control.delay);
-		} else return false;
-	}
+        this.runMacro(macro)
+          .then(() => res.status(204).send())
+          .catch((error) =>
+          (error.split(":")[0].split(" ")[0] === "invalid"
+            ? res.status(400)
+            : res.status(500)
+          ).json({ error })
+        );
+      })
+      .all((req, res) => {
+        res.status(405).send();
+      });
 
-	private isValidMacro(macro: Control[]) {
-		if (!Array.isArray(macro)) {
-			return false;
-		}
-		return macro.reduce((prev: boolean, curr: Control) => {
-			return prev && this.isValidControl(curr);
-		}, true);
-	}
+    router
+      .route("/macro/:name")
+      .get((req, res) => {
+        if (!this.config.allowed) {
+          res.status(401).json({ error: "remote control not allowed" });
+          return;
+        }
 
-	private runMacro(macro: Control[]) {
-		if (this.isValidMacro(macro)) {
-			return macro.reduce(async (previous: Promise<any>, current) => {
-				await previous;
-				return this.runControl(current);
-			}, Promise.resolve());
-		} else {
-			return Promise.reject(`invalid macro: ${JSON.stringify(macro)}`);
-		}
-	}
+        const { name: macroName } = req.params;
+        if (macroName in this.config.macros) {
+          res.json({
+            macro: this.config.macros[macroName],
+          });
+        } else {
+          res.status(404).json({ error: `macro not found: ${macroName}` });
+        }
+      })
+      .post((req, res) => {
+        if (!this.config.allowed) {
+          res.status(401).json({ error: "remote control not allowed" });
+          return;
+        }
 
-	private runControl(control: Control) {
-		if (!this.isValidControl(control)) {
-			return Promise.reject(`invalid control: ${JSON.stringify(control)}`);
-		} else if (control.type === ControlType.keyTapping) {
-			return new Promise((resolve) => {
-				robotjs.keyTap(control.key, control.modifiers);
-				resolve(undefined);
-			});
-		} else if (control.type === ControlType.stringTyping) {
-			return new Promise((resolve) => {
-				robotjs.typeString(control.string);
-				resolve(undefined);
-			});
-		} else {
-			return new Promise((resolve) => setTimeout(resolve, control.delay));
-		}
-	}
+        const { name: macroName } = req.params;
+        if (req.params.name in this.config.macros) {
+          const macro = this.config.macros[macroName];
+          this.runMacro(macro)
+            .then(() => res.status(204).send())
+            .catch((error: Error) => res.status(500).json({ error }));
+        }
+      })
+      .all((req, res) => {
+        res.status(405).send();
+      });
+
+    return router;
+  }
+
+  private isValidControl(control: Control) {
+    if (control.type === ControlType.keyTapping) {
+      return control.key !== undefined && typeof control.key === "string" && control.key !== "";
+    } else if (control.type === ControlType.stringTyping) {
+      return control.string !== undefined && typeof control.string === "string";
+    } else if (control.type === ControlType.delaying) {
+      return control.delay !== undefined && !isNaN(control.delay);
+    } else return false;
+  }
+
+  private isValidMacro(macro: Control[]) {
+    if (!Array.isArray(macro)) {
+      return false;
+    }
+    return macro.reduce((prev: boolean, curr: Control) => {
+      return prev && this.isValidControl(curr);
+    }, true);
+  }
+
+  private runMacro(macro: Control[]) {
+    if (this.isValidMacro(macro)) {
+      return macro.reduce(async (previous: Promise<any>, current) => {
+        await previous;
+        return this.runControl(current);
+      }, Promise.resolve());
+    } else {
+      return Promise.reject(`invalid macro: ${JSON.stringify(macro)}`);
+    }
+  }
+
+  private runControl(control: Control) {
+    if (!this.isValidControl(control)) {
+      return Promise.reject(`invalid control: ${JSON.stringify(control)}`);
+    } else if (control.type === ControlType.keyTapping) {
+      return new Promise((resolve) => {
+        robotjs.keyTap(control.key, control.modifiers);
+        resolve(undefined);
+      });
+    } else if (control.type === ControlType.stringTyping) {
+      return new Promise((resolve) => {
+        robotjs.typeString(control.string);
+        resolve(undefined);
+      });
+    } else {
+      return new Promise((resolve) => setTimeout(resolve, control.delay));
+    }
+  }
 }
