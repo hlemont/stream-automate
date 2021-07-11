a3c0e658dfe739d9cd8a7654aaad9323dcb07448
hlemont <hlemont01@gmail.com>
2021-07-11 14:24:44 +0900

    add interactive server console #2 reload and stop via console

--- index.ts
+++ index.ts
@@ -2,11 +2,38 @@ import express from 'express';
 import process from 'process';
 import { loadConfig } from "./app/config";
 import Server from './app/Server';
+import readline from "readline";
 
-process.title = "OBSRouter";
-const config = loadConfig();
-console.log(JSON.stringify(config, undefined, 1));
-
+const rl = readline.createInterface(process.stdin)
 const app = express();
-const server = new Server(app);
-server.start(config.general.serverPort)
+let server: Server;
+
+process.title = "StreamAutomate";
+startServer();
+
+rl.on("line", (input) => {
+  const _input = input.trim();
+  if(_input === "restart") {
+    console.log("server is rebooting...");
+    server.stop();
+    startServer();
+  } else if(_input === "stop") {
+    console.log("server is closing...");
+    server.stop();				
+    console.log("Press enter to exit");
+    rl.once("line", () => process.exit(0));
+  }
+})
+
+function startServer() {
+  try {
+    
+  }
+  // load config
+  const config = loadConfig();
+  console.log(JSON.stringify(config, undefined, 1));
+
+  // initialize and start server
+  server = new Server(app, config.general.serverPort);
+  server.start();
+}
