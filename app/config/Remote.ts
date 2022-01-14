cbb69fb0c349a11dc3478d6c960fd8f0de2b5576
hlemont <hlemont@gmail.com>
2022-01-14 22:59:05 +0900

    update config structures

--- app/config/Remote.ts
+++ app/config/Remote.ts
@@ -1,60 +1,76 @@
 import Joi from "joi";
+import { Key } from "@nut-tree/nut-js"
 
 export enum ControlType {
 	keyTapping = "key",
 	stringTyping = "string",
 	delaying = "delay",
-}
+};
 
-export const controlSchema = Joi.object().keys({
-	type: Joi.string().required().valid(
-		ControlType.keyTapping,
-		ControlType.stringTyping,
-		ControlType.delaying
-	),
-	modifiers: Joi.any().when("type", {
-		is: ControlType.keyTapping,
-		then: Joi.array().items(Joi.string()).default([]),
-	}),
-	key: Joi.string().when("type", {
-		is: ControlType.keyTapping,
-		then: Joi.required(),
-	}),
-	string: Joi.string().when("type", {
-		is: ControlType.stringTyping,
-		then: Joi.required(),
-	}),
-	delay: Joi.number().when("type", {
-		is: ControlType.delaying,
-		then: Joi.required(),
-	}),
-});
+export const controlSchema = Joi.object()
+	.keys({
+		type: Joi.string().required().valid(
+			ControlType.keyTapping,
+			ControlType.stringTyping,
+			ControlType.delaying
+		),
+		modifiers: Joi.any().when("type", {
+			is: ControlType.keyTapping,
+			then: Joi.array().items(Joi.string().valid(...Object.keys(Key)))
+				.default([]),
+		}),
+		key: Joi.string()
+			.valid(...Object.keys(Key)).when("type", {
+				is: ControlType.keyTapping,
+				then: Joi.required(),
+			}),
+		string: Joi.string().when("type", {
+			is: ControlType.stringTyping,
+			then: Joi.required(),
+		}),
+		delay: Joi.number().when("type", {
+			is: ControlType.delaying,
+			then: Joi.required(),
+		}),
+	})
+	.options({ stripUnknown: true });
 
-export const macroSchema = Joi.array().min(1).items(controlSchema);
+export const macroSchema = Joi.object()
+	.keys({
+		controls: Joi.array()
+			.min(1)
+			.items(controlSchema)
+	})
+	.options({ stripUnknown: true });
 
-export const configSchema = Joi.object().keys({
-	allowed: Joi.boolean().required(),
-	macros: Joi.array()
-		.unique((a, b) => a.name === b.name)
-		.items(Joi.object().keys({ name: Joi.string(), macro: macroSchema })),
-});
+
+export const configSchema = Joi.object()
+	.keys({
+		allowed: Joi.boolean().required(),
+		macros: Joi.array()
+			.unique((a, b) => a.name === b.name)
+			.items(Joi.object().keys({ name: Joi.string(), macro: macroSchema })),
+	})
+	.options({ stripUnknown: true });
 
 export type Control =
 	| {
-			type: ControlType.keyTapping;
-			key: string;
-			modifiers: string[];
-	  }
+		type: ControlType.keyTapping;
+		key: string;
+		modifiers: string[]
+	}
 	| {
-			type: ControlType.stringTyping;
-			string: string;
-	  }
+		type: ControlType.stringTyping;
+		string: string;
+	}
 	| {
-			type: ControlType.delaying;
-			delay: number;
-	  };
+		type: ControlType.delaying;
+		delay: number;
+	}
 
-export type Macro = Control[];
+export interface Macro {
+	controls: Control[]
+};
 
 export interface Config {
 	allowed: boolean;
@@ -62,4 +78,4 @@ export interface Config {
 		name: string;
 		macro: Macro;
 	}[];
-}
+};
